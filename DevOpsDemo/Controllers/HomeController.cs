c9320513778105fefe438bc56c035ebe048ce548
Shiva Patil <shivupatil035@gmail.com>
2023-02-15 01:49:05 +0530

    Add project files.

---
+++ DevOpsDemo/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DevOpsDemo.Repository;
+using DevOpsDemo.Models;
+using System.Diagnostics;
+
+namespace DevOpsDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        IPostRepository postRepository;
+        public HomeController(IPostRepository _postRepository)
+        {
+            postRepository = _postRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var model = postRepository.GetPosts();
+            return View(model);
+        }
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None,
+       NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel
+            {
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            });
+        }
+    }
+}
